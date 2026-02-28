0cd1c1e6106df6e0f73bae6b202a6d911944509e
doghouser11 <doghouser11@gmail.com>
2026-02-28 08:28:50 +0200

    Update api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -9,4 +9,28 @@ const api = axios.create({
   },
 });
 
+// Експортираме базовия обект по подразбиране
 export default api;
+
+// Дефинираме и експортираме липсващите обекти, които Next.js търси:
+export const activitiesAPI = {
+  getAll: () => api.get('/activities'),
+  getOne: (id: any) => api.get(`/activities/${id}`),
+};
+
+export const schoolsAPI = {
+  getAll: () => api.get('/schools'),
+  getOne: (id: any) => api.get(`/schools/${id}`),
+};
+
+export const neighborhoodsAPI = {
+  getAll: () => api.get('/neighborhoods'),
+};
+
+export const leadsAPI = {
+  getAll: () => api.get('/leads'),
+};
+
+export const reviewsAPI = {
+  getAll: () => api.get('/reviews'),
+};
